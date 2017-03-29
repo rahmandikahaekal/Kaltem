97fdf79c09278ea3af055c8c9c5159193e4eb8de
rahmandika haekal <rahmandikahaekal31@gmail.com>
2017-03-29 21:14:47 +0700

    progress blog.html

--- js/main_blog.js
+++ js/main_blog.js
@@ -14,6 +14,37 @@ new WOW().init();
         });
         
         //Navigation Change' Color
+        $(window).on('scroll', function () {
+            if ($(window).scrollTop() > 10) {
+                $('.header-top-area').addClass('menu-bg');
+                jQuery("#logo-dark").css({"display": "inline"});
+                jQuery("#logo-white").css({"display": "none"});
+            } else {""
+                $('.header-top-area').removeClass('menu-bg');
+                jQuery("#logo-dark").css({"display": "none"});
+                jQuery("#logo-white").css({"display": "inline"})
+            }
+        });
         
-    })
+        //Smoothscroll
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+        
+        //Extra JS
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+        
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        });
+    });
 })(jQuery);
